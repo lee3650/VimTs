0590909064685d751e6ebd79b855f5d969b7938a
Isaac <lee3650@purdue.edu>
2023-09-01 23:00:47 -0400

    Started app implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import './App.css'
+import React, {useState} from 'react'
 import Vim from './model/Vim'; 
+import VimOutput from './model/VimOutput';
+
+const startingStr = 'Hello Vim!\nTry moving the cursor.'
 
 function App() {
 
-  myvim = new Vim("example")
+  const [state, setState] = useState<string[]>(new VimOutput(startingStr.split('\n'), ))
+  const [vim, setVim] = useState<Vim>(new Vim(startingStr))
+
+  let handleKey = (ev : KeyboardEvent) => {
+    let key = ev.key
+    let output = vim.execute(key)
+    setText(output.text)
+  }
 
   return <html></html>
 }
