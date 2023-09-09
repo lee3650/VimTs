080c135a464251ff0e592dc9bda651d3a578224c
SMason1441 <mason142@purdue.edu>
2023-09-08 22:31:36 -0400

    Added fully functional hjkl movements

--- src/model/Vim.test.ts
+++ src/model/Vim.test.ts
@@ -2,9 +2,15 @@ import {expect, test} from 'vitest'
 import Vim from "./Vim";
 import Point from './Point';
 
-test('move cursor left works', () => {
+test('move cursor right works', () => {
     const myvim = new Vim('abcdefg'); 
     const output = myvim.execute('l'); 
     expect(output.cursorPos).toEqual(new Point(0, 1)); 
+})
+
+test('move cursor left works', () => {
+    const myvim = new Vim('abcdefg'); 
+    const output = myvim.execute('r'); 
+    expect(output.cursorPos).toEqual(new Point(0, 0)); 
 })
 
