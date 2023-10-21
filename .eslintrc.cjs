fad5ec98e960b4174b0b29de746358039d283b53
chi-wei-lien <wilsonwilson3124@gmail.com>
2023-10-20 21:56:30 -0400

    adjust lint command

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,11 +8,14 @@ module.exports = {
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'prettier', 'unused-imports'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'prettier/prettier': ['error'],
+    'unused-imports/no-unused-imports': ['error'],
+    'no-console': ['error']
   },
 }
