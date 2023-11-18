08165f374a3a4062a86e8359fefad06aabb02c79
Isaac <lee3650@purdue.edu>
2023-11-17 21:15:33 -0500

    test log

--- src/model/Utility.ts
+++ src/model/Utility.ts
@@ -18,6 +18,7 @@ export function insertText(
   if (newLine) {
     text.splice(curPos.row, 0, ...newText)
     curPos.col = 0
+    console.log('test')
     curPos.row = curPos.row + 1
   }
   //TODO
